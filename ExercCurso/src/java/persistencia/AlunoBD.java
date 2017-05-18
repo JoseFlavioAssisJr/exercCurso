9d9f3a0bb26ab4f83310644e91b47404f55a0a0a
JoseFlavioAssisJr <joseflaviojr@outlook.com>
2017-05-17 22:23:51 -0300

    adiciona/remove/buscaAlunos

--- ExercCurso/src/java/persistencia/AlunoBD.java
+++ ExercCurso/src/java/persistencia/AlunoBD.java
@@ -9,14 +9,56 @@ import java.util.List;
  * @author alunoces
  */
 public class AlunoBD {
-    
+
     private List<Aluno> alunos;
 
     public AlunoBD() {
         alunos = new ArrayList<Aluno>();
     }
-    
-    public void adicionarAluno(Aluno aluno){
+
+    public void adicionarAluno(Aluno aluno) {
         alunos.add(aluno);
     }
+
+    public void removerAluno(Aluno aluno) {
+        alunos.remove(aluno);
+    }
+
+    public Aluno buscarAluno(String matricula) {
+        int i;
+        boolean achou = false;
+        Aluno a1 = null;
+
+        for (Aluno a : alunos) {
+            if (a.getMatricula() == matricula) {
+                a1 = a;
+                break;
+           
+        }
+        }
+    
+        if (a1 != null) {
+            return a1;
+            else
+            return null;
+        }
+        
+    }
+        
+        //        for (i = 0; i < alunos.size(); i++) {
+        //            if (alunos.get(i).getMatricula() == matricula) {
+        //                achou = true;
+        //                break;
+        //            }
+        //        }
+        //        if (achou) {
+        //            return alunos.get(i);
+        //        } else {
+        //            return null;
+        //        }
+
+    public List<Aluno> buscarTodos() {
+        return alunos;
+    }
+
 }
